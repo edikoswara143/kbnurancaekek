7024a6ef789a395a8c7d201f8959d34e467dbb2f
Edi Koswara <edikoswara143@gmail.com>
2024-01-09 19:53:24 +0700

    hello

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="mx-4 dekstop:mx-12">
-      <p className="text-emerald-600 font-bold">Hello World !!!</p>
+      <p className="text-emerald-600 font-bold">KBNU RANCAEKEK!!!</p>
     </div>
   );
 }
